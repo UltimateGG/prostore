0cc677850ccb361d1cc82866e73c275a1a63064d
UltimateGG <clubblock1@gmail.com>
2024-12-23 21:51:50 -0600

    Responsive nav menu

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,7 @@
-import { Button } from '@/components/ui/button';
 import { APP_NAME } from '@/lib/constants';
-import { ShoppingCart, UserIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
-import ThemeToggle from './theme-toggle';
+import Menu from './menu';
 
 const Header = () => {
   return (
@@ -16,21 +14,7 @@ const Header = () => {
           </Link>
         </div>
 
-        <div className="space-x-2">
-          <ThemeToggle />
-
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingCart /> Cart
-            </Link>
-          </Button>
-
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon /> Sign In
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   );
