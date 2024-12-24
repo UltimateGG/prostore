0e7ad41c7ad92ec7ce478e76c2209b7f4f862aa4
UltimateGG <clubblock1@gmail.com>
2024-12-23 20:15:10 -0600

    Add ShadCN

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from '@/components/ui/button';
+
 const Homepage = () => {
-  return <>Prostore</>;
+  return (
+    <>
+      Prostore
+      <Button variant="destructive">Test Btn</Button>
+    </>
+  );
 };
 
 export default Homepage;
